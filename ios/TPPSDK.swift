9c4ee310825c04a081e08dd2ad1525b50bfd6f1e
Parker Brooks <jonparkerbrooks@gmail.com>
2024-04-20 11:47:44 -0500

    added callback gaurd + debug info on bridge

--- ios/TPPSDK.swift
+++ ios/TPPSDK.swift
@@ -5,53 +5,64 @@ import MagTekSDK
 
 @objc(TPPSDK)
 class TPPSDK:  RCTEventEmitter {
+  private static var reader: MagTekCardReader?
+  private var transactionCallbackCalled: Bool = false
 
-    private static var reader: MagTekCardReader?
+  @objc func initialize(_ apiKey: String) {
+    TPPSDK.reader = MagTekCardReader(apiKey, debug: true)
+  }
 
-    @objc func initialize(_ apiKey: String) {
-        TPPSDK.reader = MagTekCardReader(apiKey, debug: true)
-    }
+  @objc override static func requiresMainQueueSetup() -> Bool {
+    return true
+  }
 
-    @objc override static func requiresMainQueueSetup() -> Bool {
-        return true
-    }
-
-    override func supportedEvents() -> [String]! {
+  override func supportedEvents() -> [String]! {
     return []
-}
+  }
 
-    @objc func startDeviceDiscovery(_ callback: @escaping RCTResponseSenderBlock) {
-        TPPSDK.reader?.startDeviceDiscovery { name, rssi in
-            callback([["name": name, "rssi": rssi]])
-        }
+  @objc func startDeviceDiscovery(_ callback: @escaping RCTResponseSenderBlock) {
+    TPPSDK.reader?.startDeviceDiscovery { name, rssi in
+      callback([["name": name, "rssi": rssi]])
     }
+  }
 
-    @objc func cancelDeviceDiscovery() {
-        TPPSDK.reader?.cancelDeviceDiscovery()
-    }
+  @objc func cancelDeviceDiscovery() {
+    TPPSDK.reader?.cancelDeviceDiscovery()
+  }
 
-    @objc func connect(_ deviceName: String, timeout: TimeInterval, callback: @escaping RCTResponseSenderBlock) {
-        TPPSDK.reader?.connect(deviceName, timeout: timeout) { connected in
-            callback([connected])
-        }
+  @objc func connect(_ deviceName: String, timeout: TimeInterval, callback: @escaping RCTResponseSenderBlock) {
+    TPPSDK.reader?.connect(deviceName, timeout: timeout) { connected in
+      callback([connected])
     }
+  }
 
-    @objc func disconnect() {
-        TPPSDK.reader?.disconnect()
-    }
+  @objc func disconnect() {
+    TPPSDK.reader?.disconnect()
+  }
 
-    @objc func startTransaction(_ amount: String, callback: @escaping RCTResponseSenderBlock) {
-        TPPSDK.reader?.startTransaction(amount) { [weak self] message, event, status in
-          callback([["message": message, "event": MagTekCardReader.getEventMessage(event), "status": MagTekCardReader.getStatusMessage(status)]])
-        }
+  @objc func startTransaction(_ amount: String, callback: @escaping RCTResponseSenderBlock) {
+    if (transactionCallbackCalled) {
+      print("startTransaction was called again!")
+      print("amount: \(amount)")
+      print("callback: \(callback)")
+    } else {
+      transactionCallbackCalled = true;
+      TPPSDK.reader?.startTransaction(amount) { [weak self] message, event, status in
+        callback([[
+          "message": message,
+          "event": MagTekCardReader.getEventMessage(event),
+          "status": MagTekCardReader.getStatusMessage(status)
+        ]])
+      }
     }
+  }
 
-    @objc func getSerialNumber(_ callback: RCTResponseSenderBlock) {
-        let serialNumber = TPPSDK.reader?.getSerialNumber() ?? ""
-        callback([serialNumber])
-    }
+  @objc func getSerialNumber(_ callback: RCTResponseSenderBlock) {
+    let serialNumber = TPPSDK.reader?.getSerialNumber() ?? ""
+    callback([serialNumber])
+  }
 
-    @objc func cancelTransaction() {
-        TPPSDK.reader?.cancelTransaction()
-    }
+  @objc func cancelTransaction() {
+    TPPSDK.reader?.cancelTransaction()
+  }
 }
